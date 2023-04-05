cd9eaaa1faad3e8b931c05195191f7f287d77ee4
Denny Azevedo <cg080123@LAB117PROF.IFSP>
2023-04-04 22:33:05 -0300

    Autenticando - cadastrando usuário

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const AuthValidator = require('./validators/AuthValidator');
+
 const AuthController = require('./controllers/AuthController');
 
 router.get('/ping', (req, res) => {
@@ -8,6 +10,6 @@ router.get('/ping', (req, res) => {
 });
 
 router.post('/user/signin', AuthController.signin);
-router.post('/user/signup', AuthController.signup);
+router.post('/user/signup', AuthValidator.signup, AuthController.signup);
 
 module.exports = router;
