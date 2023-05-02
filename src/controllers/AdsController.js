68babce4bcf6b58c2c1fbc6ffd1d6a50604e6808
Denny Azevedo <cg080123@LAB111PROF.IFSP>
2023-05-02 20:35:21 -0300

    Terminado o AddAction de Anuncios

--- src/controllers/AdsController.js
+++ src/controllers/AdsController.js
@@ -38,5 +38,57 @@ module.exports = {
       res.json({ error: 'ID de categoria inválido' });
       return;
     }
-  }
+    const category = await Category.findById(cat);
+    if(!category) {
+      res.json({ error: 'Categoria inexistente'});
+      return;
+    }
+    // R$ 8.000,35 => 8000.35
+    if (price) {
+      price = price
+              .replace('.','')
+              .replace(',','.')
+              .replace('R$', '');
+      price = parseFloat(price);
+    } else {
+      price = 0;
+    }
+    const newAd = new Ad();
+    newAd.status = true;
+    newAd.idUser = user._id;
+    newAd.state = user.state;
+    newAd.dateCreated = new Date();
+    newAd.title = title;
+    newAd.category = cat;
+    newAd.price = price;
+    newAd.priceNegotiable = (priceneg == 'true') ? true : false;
+    newAd.description = desc;
+    newAd.views = 0;
+    if( req.files && req.files.img) {
+      if(req.files.img.length == undefined) {
+        if(['image/jpeg','image/jpg','image/png'].includes(req.files.img.mimetype)) {
+          let url = await addImage(req.files.img.data);
+          newAd.images.push({
+            url,
+            default: false
+          });
+        }
+      } else {
+        for (let i = 0; i < req.files.img.lenght; i++) {
+          if(['image/jpeg','image/jpg','image/png'].includes(req.files.img[i].mimetype)) {
+            let url = await addImage(req.files.img[i].data);
+            newAd.images.push({
+              url,
+              default: false
+            });
+          }
+        }
+      }
+    }
+    if (newAd.images.length > 0) {
+      newAd.images[0].default = true;
+    }
+    const info = await newAd.save();
+    res.json({ id: info._id });
+  },
 }
